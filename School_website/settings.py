175978cb800d967b7efd8c6cba7dab1b2d6708e2
futuregodwin <164335576+futuregodwin@users.noreply.github.com>
2025-08-20 10:53:44 +0100

    Update settings django browser reload.py

--- School_website/settings.py
+++ School_website/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'teachers_app.apps.TeachersAppConfig',
     'tailwind',
     'theme',
-    'django_browser_reload',
+    # 'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    # "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 
