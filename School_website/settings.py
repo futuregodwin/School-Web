a9a3517b73367e5294ebb20bd72e238f82ca619d
futuregodwin <164335576+futuregodwin@users.noreply.github.com>
2025-08-20 09:21:32 +0100

    Update settings.py

--- School_website/settings.py
+++ School_website/settings.py
@@ -45,7 +45,7 @@ INTERNAL_IPS = [
 
 ]
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
