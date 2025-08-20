3c5b4075a9dac490ff7f64db2dcffccee1e856a4
futuregodwin <164335576+futuregodwin@users.noreply.github.com>
2025-08-20 10:04:20 +0100

    Update settings.py

--- School_website/settings.py
+++ School_website/settings.py
@@ -145,8 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/theme/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- add this
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 MEDIA_URL = '/media/'
